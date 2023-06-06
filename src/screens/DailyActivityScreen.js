4ac0471de4d24e50e3b5881e828db317046b0319
Rkenneth13 <it.russelkennethregala@gmail.com>
2023-06-07 02:20:41 +0800

    done task

--- src/screens/DailyActivityScreen.js
+++ src/screens/DailyActivityScreen.js
@@ -10,38 +10,376 @@ import {
 export default function DailyActivityScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={styles.contentContainer}>
-        <Text>DailyActivityScreen</Text>
-      </View>
+    <View style={styles.contentContainer}>
+    <TouchableOpacity>
+        <Image
+          source={require("../../assets/home.png")}
+          style={styles.image5}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image
+          source={require("../../assets/tropy.png")}
+          style={styles.image2}
+        />
+        <Text style={styles.text}>My Trophies</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image
+          source={require("../../assets/award.png")}
+          style={styles.image}
+        />
+        <Text style={styles.text1}>Achievements</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image
+          source={require("../../assets/setting.png")}
+          style={styles.image1}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image
+          source={require("../../assets/user.png")}
+          style={styles.image4}
+        />
+             </TouchableOpacity>
+           <TouchableOpacity >
+        <Text style={styles.button}>    Daily Activity</Text>
+      </TouchableOpacity>
+ 
+      <TouchableOpacity >
+        <Text style={styles.text3}>    DAY 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity}>  Activity</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity1}>     Add+</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.text4}>    DAY 2</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity}>  Activity</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity1}>   Activity</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity2}>   Activity</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity3}>     Add+</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+
+        <Text style={styles.text5}>    DAY 3  </Text>
+         
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity}>  Activity</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity1}>   Activity</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity2}>   Activity</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.textactivity3}>     Add+</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+       
+         {/* <Text style={styles.textnames1}>Talking With</Text> */}
+         {/* <Text style={styles.textnames1}>   Friends</Text> */}
+      </TouchableOpacity>
+
+      <View style={styles.contentContainer}></View>
     </View>
+  </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#E7BD6D",
+    backgroundColor: "#8639DB",
     marginTop: 20,
   },
   contentContainer: {
     width: "90%",
+    justifyContent: "center",
+    padding: 10,
+    marginTop: 100,
+    fontSize: 200,
+    fontWeight: "bold",
+  },
+  text3: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: -4,
+    marginTop: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#C6E2FF",
+    width: 120,
+    flexDirection: "row",
+    height: 50,
+    padding: 10,
+  },
+  textactivity: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 130,
+    marginTop: -50,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#fff",
-    padding: 20,
-    marginTop: 50,
+    width: 120,
+    flexDirection: "row",
+    height: 50,
+    padding: 10,
+  },
+  textactivity1: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 265,
+    marginTop: -50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    width: 120,
+    flexDirection: "row",
+    height: 50,
+    padding: 10,
+  },
+  textactivity2: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 400,
+    marginTop: -50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    width: 120,
+    flexDirection: "row",
+    height: 50,
+    padding: 10,
+  },
+  textactivity3: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 535,
+    marginTop: -50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    width: 120,
+    flexDirection: "row",
+    height: 50,
+    padding: 10,
+  },
+  text4: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: -4,
+    marginTop:10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#C6E2FF",
+    width: 120,
+    padding: 10,
+    height: 50,
+  },
+  text5: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: -4,
+    marginTop:10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#C6E2FF",
+    width: 120,
+    flexDirection: "row",
+    height: 50,
+    padding: 10,
+  },
+  text6: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 510,
+    marginTop: -150,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 150,
+    flexDirection: "row",
+    height: 150,
+    backgroundColor: "#E7BD6D",
+  },
+  textnames: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 50,
+    marginTop: 1,
+  },
+  textnames1: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginLeft: 180,
+    marginTop: 1,
+  },
+  textnames2: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 50,
+    marginTop: 1,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 190,
+    marginTop: -40,
+  },
+  
+
+  text1: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 355,
+    marginTop: -40,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 20,
+    marginTop: 10,
+    textAlign: "center",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+    padding: 10,
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  image: {
+    width: 70,
+    height: 70,
+  },
+  imageContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+  },
+  button: {
+    backgroundColor: "#C6E2FF",
     borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
-    marginBottom: 60,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom:-30,
+    width: "20%",
+    flexDirection: "row",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+
+
+  image: {
+    position: "absolute",
+    flex: 1,
+    width: 30,
+    height: 46,
+    marginLeft: 320,
+    marginTop: -50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image2: {
+    position: "absolute",
+    flex: 1,
+    width: 37,
+    height: 36,
+    marginLeft: 150,
+    marginTop: -45,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image5: {
+    position: "absolute",
+    flex: 1,
+    width: 37,
+    height: 36,
+    marginLeft: 1,
+    marginTop: -45,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+
+  image1: {
+    position: "absolute",
+    flex: 1,
+    width: 45,
+    height: 45,
+    marginLeft: 570,
+    marginTop: -50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image4: {
+    position: "absolute",
+    flex: 1,
+    width: 50,
+    height: 50,
+    marginLeft: 500,
+    marginTop: -50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  images: {
+    position: "absolute",
+    flex: 1,
+    width: 150,
+    height: 100,
+    marginLeft: 650,
+    marginTop: -100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  images1: {
+    position: "absolute",
+    flex: 1,
+    width: 150,
+    height: 100,
+    marginLeft: 650,
+    marginTop: -100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  images2: {
+    position: "absolute",
+    flex: 1,
+    width: 150,
+    height: 100,
+    marginLeft: 650,
+    marginTop: -100,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
